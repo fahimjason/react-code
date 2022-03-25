b8679b27d3f1c745b72ff07828ba359328aed6c7
JisanFahim <fahimjason@users.noreply.github.com>
2022-03-26 01:56:55 +0600

    lifting-the-state-up

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+
+import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
 const App = () => {
@@ -9,12 +11,7 @@ const App = () => {
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    {
-      id: 'e2',
-      title: 'New TV',
-      amount: 799.49,
-      date: new Date(2021, 2, 12)
-    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
     {
       id: 'e3',
       title: 'Car Insurance',
@@ -29,13 +26,24 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = expense => {
+    console.log('In App.js');
+    console.log(expense);
+  };
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
-  // return React.createElement('div', {}, React.createElement('h2', {}, 'Let\'s get started!'));
 }
 
 export default App;
