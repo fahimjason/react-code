44be527954777a46d9341b932fc0a447098ac99a
JisanFahim <fahimjason@users.noreply.github.com>
2022-03-28 21:13:33 +0600

    Solved confliged issues.

--- src/index.js
+++ src/index.js
@@ -5,13 +5,9 @@ import './index.css';
 import App from './App';
 import { AuthContextProvider } from './store/auth-context';
 
-<<<<<<< HEAD
-ReactDOM.render(<App />, document.getElementById('root'));
-=======
 ReactDOM.render(
     <AuthContextProvider>
         <App />
     </AuthContextProvider>,
     document.getElementById('root')
 );
->>>>>>> 08-side-effects-reducers-context-api
