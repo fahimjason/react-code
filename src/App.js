f3fb221bd795d56c7d044b540b66159c01e3ac88
JisanFahim <fahimjason@users.noreply.github.com>
2022-04-03 19:42:46 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import BasicForm from './components/BasicForm';
+
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
+    <div className="app">
+      <BasicForm />
     </div>
   );
 }
